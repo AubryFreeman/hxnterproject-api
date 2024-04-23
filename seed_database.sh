9e3dfe9cf5e8b287c1677eaf86e6d2ae4b51fcca
Aubry Freeman <aubryfreeman23@gmail.com>
2024-04-23 14:27:15 -0500

    Initial commit

--- seed_database.sh
+++ seed_database.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm db.sqlite3
-rm -rf ./hunterapi/migrations
+rm -rf ./hxnterapiapi/migrations
 python3 manage.py migrate
-python3 manage.py makemigrations hunterapi
-python3 manage.py migrate hunterapi
+python3 manage.py makemigrations hxnterapiapi
+python3 manage.py migrate hxnterapiapi
 python3 manage.py loaddata users
 python3 manage.py loaddata tokens
 
